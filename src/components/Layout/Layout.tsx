38215a60bd7b3aed28eeef332ae8fcd7c1ef256f
Fahad MB <fahadmbasheer@gmail.com>
2026-07-20 16:31:36 +0530

    Updated

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,12 @@
 import Navbar from "../Navbar/Navbar";
+import {Outlet} from 'react-router-dom';
+
 
 function Layout() {
   return (
     <>
       <Navbar />
+      <Outlet />
     </>
   );
 }
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,13 +2,15 @@ import { Routes , Route } from "react-router-dom";
 
 
 import Home from "../pages/Home/Home";
-
+import Layout from "../components/Layout/Layout";
 
 
 function AppRoutes () {
     return (
         <Routes>
-            <Route path="/" element={<Home />}/>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />}/>
+            </Route>
         </Routes>
     )
 }
