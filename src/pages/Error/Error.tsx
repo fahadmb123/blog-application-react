83465fb402e8286c02e36c4b15e571ed8377a185
Fahad MB <fahadmbasheer@gmail.com>
2026-07-20 15:05:09 +0530

    Error added

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,15 @@
+import "./Error.css";
+
+function Error() {
+  return (
+    <div className="error">
+
+      <h1>404</h1>
+
+      <p>Page Not Found</p>
+
+    </div>
+  );
+}
+
+export default Error;
