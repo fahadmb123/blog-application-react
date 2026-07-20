4ede6bff280600314892ce480bffa55f96616a68
Fahad MB <fahadmbasheer@gmail.com>
2026-07-20 15:00:25 +0530

    Home Updated

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to My Blog</h1>
+
+      <p>Read amazing blogs from different authors.</p>
+
+      <button>Explore Blogs</button>
+    </div>
+  );
+}
+
+export default Home;
