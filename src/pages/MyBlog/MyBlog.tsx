7cad630149f598c2977b615263cfff6f547cd49c
Fahad MB <fahadmbasheer@gmail.com>
2026-07-20 15:04:00 +0530

    My Blog Addded

---
+++ src/pages/MyBlog/MyBlog.tsx
@@ -0,0 +1,25 @@
+import "./MyBlog.css";
+
+function MyBlog() {
+  return (
+    <div className="myBlog">
+
+      <h1>My Blogs</h1>
+
+      <div className="blogCard">
+
+        <h2>My First Blog</h2>
+
+        <p>
+          This is a sample blog description.
+        </p>
+
+        <button>Edit</button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default MyBlog;
