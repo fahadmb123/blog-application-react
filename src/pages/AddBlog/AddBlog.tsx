b6639be1f03b210001e8c44a03eca547bff835e4
Fahad MB <fahadmbasheer@gmail.com>
2026-07-20 15:02:00 +0530

    Add Blog Added

---
+++ src/pages/AddBlog/AddBlog.tsx
@@ -0,0 +1,26 @@
+import "./AddBlog.css";
+
+function AddBlog() {
+  return (
+    <div className="addBlog">
+      <h1>Add Blog</h1>
+
+      <form>
+
+        <input
+          type="text"
+          placeholder="Blog Title"
+        />
+
+        <textarea
+          placeholder="Write your blog..."
+        ></textarea>
+
+        <button>Add Blog</button>
+
+      </form>
+    </div>
+  );
+}
+
+export default AddBlog;
