a3d5c0d6a4255ffcecfceaeea9fbb98e17155b84
Fahad MB <fahadmbasheer@gmail.com>
2026-07-21 21:08:30 +0530

    Service Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
+import { UserProvider } from './providers/AuthProvider'
+
 
 function App() {
   
 
   return (
     <BrowserRouter>
+    <UserProvider>
        <AppRoutes/>
+    </UserProvider>
     </BrowserRouter>
   )
 }
---
+++ src/services/AuthService.ts
@@ -0,0 +1,8 @@
+import type { User } from "../types/auth";
+
+
+
+
+export function signup(user:User){
+    
+}
