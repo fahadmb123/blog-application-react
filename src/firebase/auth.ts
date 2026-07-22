14d8dc241a52cd2ca94d7c8a846a4bbb13c9034a
Fahad MB <fahadmbasheer@gmail.com>
2026-07-22 20:07:25 +0530

    Auth Updated on the Firebase

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,9 +1,27 @@
 import { createUserWithEmailAndPassword } from "firebase/auth"
 import type { User } from "../types/auth"
-import { db } from "../firebase/config"
+import { auth } from "../firebase/config"
+import { FirebaseError } from "firebase/app"
+
+
 
 
 export const addUser = async (newUser:User)=>{
-    const returned = await createUserWithEmailAndPassword(db,newUser.email,newUser.password)
-    console.log(returned)
+    try {
+        const returned = await createUserWithEmailAndPassword(auth,newUser.email,newUser.password)
+        console.log(returned.user)
+    } catch (err) {
+        if (err instanceof FirebaseError){
+            const message = err.code.replace("auth/","").replaceAll("-"," ")
+            return {
+                did : false,
+                message
+            }
+        }
+        return {
+            did : false,
+            message: "Somthing Happened"
+        }
+    }
+    
 }
