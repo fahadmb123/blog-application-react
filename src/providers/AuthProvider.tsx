58daa72cb05da4e6f25e9e80869499ba87f8b820
Fahad MB <fahadmbasheer@gmail.com>
2026-07-23 22:30:25 +0530

    Fix

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -19,7 +19,7 @@ export function UserProvider({ children }: UserProviderProps) {
         setUser(undefined)
       }
       setLoad(false)
-      return result
+      return () => result
     })
   },[])
 
