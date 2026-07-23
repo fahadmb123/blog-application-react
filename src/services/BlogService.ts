4f07f25e9fb4a5a2518bc21083b51dfc45210f2e
Fahad MB <fahadmbasheer@gmail.com>
2026-07-23 10:07:28 +0530

    Get My Blog Service added

--- src/services/BlogService.ts
+++ src/services/BlogService.ts
@@ -1,4 +1,4 @@
-import { addBlog } from "../repositories/blogRepository";
+import { addBlog, getMyBlogs } from "../repositories/blogRepository";
 import type { BlogType } from "../types/auth";
 
 
@@ -6,3 +6,8 @@ export const newBlog = async (blog:BlogType,userId:string)=>{
     await addBlog(blog,userId)
     return {message:"Blog added successfully"}
 } 
+
+export const loadMyBlogs = async (userId:string)=>{
+    const fetch = await getMyBlogs(userId)
+    console.log(fetch)
+}
