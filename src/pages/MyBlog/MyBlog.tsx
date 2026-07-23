01c429835197110eda6d51762a545fdec68e945d
Fahad MB <fahadmbasheer@gmail.com>
2026-07-23 11:11:54 +0530

    Updated

--- src/pages/MyBlog/MyBlog.tsx
+++ src/pages/MyBlog/MyBlog.tsx
@@ -1,10 +1,23 @@
 import "./MyBlog.css";
 import BlogCard from "../../components/Blog/BlogCard";
+import { useEffect, useState } from "react";
+import { getMyBlogs } from "../../repositories/blogRepository";
+import { useUserContext } from "../../context/AuthContext";
 
 
 function MyBlog() {
   const description = "This is the Description"
   const title = "This is the Big title"
+  const [blogs,setBlogs] = useState([])
+  const {user} = useUserContext()
+  useEffect(()=>{
+    const work = async ()=>{
+      if (!user) return
+      const fetch = await getMyBlogs(user?.uid)
+      console.log(fetch)
+    }
+    work()
+  },[])
   return (
     <div className="myBlog">
 
